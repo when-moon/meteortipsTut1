8faff471e5ee8e5cbd388c5914574edf47c862f3
SoIidarity <chrism@saicomvoice.co.za>
2018-01-21 12:00:30 +0200

    end of first tutorial

---
+++ leaderboard/leaderboard.js
@@ -0,0 +1,88 @@
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+    Template.leaderboard.helpers({
+        'player': function(){
+            var currentUserId = Meteor.userId();
+            return PlayersList.find({ createdBy: currentUserId },
+                { sort: {score: -1, name: 1} });
+        },
+        'selectedClass': function(){
+            var playerId = this._id;
+            var selectedPlayer = Session.get('selectedPlayer');
+            if(playerId == selectedPlayer){
+                return "selected"
+            }
+        },
+        'selectedPlayer': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            return PlayersList.findOne({ _id: selectedPlayer });
+        }
+    });
+    Template.leaderboard.events({
+        'click .player': function(){
+            var playerId = this._id;
+            Session.set('selectedPlayer', playerId);
+        },
+        'click .increment': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            Meteor.call('updateScore', selectedPlayer, 5);
+        },
+        'click .decrement': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            Meteor.call('updateScore', selectedPlayer, -5);
+        },
+        'click .remove': function(){
+            var selectedPlayer = Session.get('selectedPlayer');
+            Meteor.call('removePlayer', selectedPlayer);
+        }
+    });
+    Template.addPlayerForm.events({
+        'submit form': function(){
+            event.preventDefault();
+            var playerNameVar = event.target.playerName.value;
+            Meteor.call('createPlayer', playerNameVar);
+            event.target.playerName.value = "";
+        }
+    });
+
+    Meteor.subscribe('thePlayers');
+
+}
+
+if(Meteor.isServer){
+    Meteor.publish('thePlayers', function(){
+        var currentUserId = this.userId;
+        return PlayersList.find({ createdBy: currentUserId });
+    });
+}
+
+Meteor.methods({
+    'createPlayer': function(playerNameVar){
+        check(playerNameVar, String);
+        var currentUserId = Meteor.userId();
+        if(currentUserId){
+            PlayersList.insert({
+                name: playerNameVar,
+                score: 0,
+                createdBy: currentUserId
+            });
+        }
+    },
+    'removePlayer': function(selectedPlayer){
+        check(selectedPlayer, String);
+        var currentUserId = Meteor.userId();
+        if(currentUserId){
+            PlayersList.remove({ _id: selectedPlayer, createdBy: currentUserId });
+        }
+    },
+    'updateScore': function(selectedPlayer, scoreValue){
+        check(selectedPlayer, String);
+        check(scoreValue, Number);
+        var currentUserId = Meteor.userId();
+        if(currentUserId){
+            PlayersList.update( { _id: selectedPlayer, createdBy: currentUserId },
+                { $inc: {score: scoreValue} });
+        }
+    }
+});
